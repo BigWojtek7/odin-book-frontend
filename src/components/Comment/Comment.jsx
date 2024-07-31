b09c440717edf2b61886232c21a63f9cdab3077b
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-07-31 13:57:21 +0200

    Add comment component to Post page

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,7 +1,14 @@
 import styles from './Comment.module.css'
 function Comment() {
   return (
-    <div>Comment</div>
+    <div className={styles.comment}>
+      <div><img src="https://i.pravatar.cc/45" alt="avatar" /></div>
+      <div className={styles.commentMain}>
+        <p className={styles.commentName}>James Smith</p>
+        <p className={styles.commentContent}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum tempore adipisci quaerat eligendi corrupti!</p>
+        <p className={styles.commentDate}>18 January at 10:00 AM</p>
+      </div>
+    </div>
   )
 }
 export default Comment
