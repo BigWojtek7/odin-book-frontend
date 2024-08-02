8d0a2d77ffd2872bfefdb19a38dffd5a61019f8a
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-02 08:53:34 +0200

    Restyle profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,15 @@
-import Post from "../../components/Post/Post"
-function Posts() {
+import Post from '../../components/Post/Post';
+import UserCard from '../../components/UserCard/UserCard';
+import styles from './Profile.module.css';
+function Profile() {
   return (
-    <Post />
-  )
+    <div className={styles.profile}>
+      <UserCard />
+      <div className={styles.posts}>
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
-export default Posts
+export default Profile;
