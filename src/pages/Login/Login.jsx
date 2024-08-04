488f740af1e8f0194c12159e7ce91bd53ce71376
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-04 18:05:32 +0200

    Add Guest mode button to view site without create account

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,21 @@
-import Input from '../../components/Form/Input';
-import Button from '../../components/Form/Buttons/SubmitButton';
-import styles from '../../components/Form/Form.module.css';
+import Input from '../../components/Form/Input/Input';
+import SubmitButton from '../../components/Form/Buttons/SubmitButton';
+import CancelButton from '../../components/Form/Buttons/cancelButton';
+import styles from './Login.module.css';
 
 function Login() {
   return (
-    <form className={styles.form}>
-      <Input type="text" name="username" labelName="Username / E-mail" />
-      <Input type="password" name="password" labelName="Password" />
-      <Button type="submit" name="Submit" />
-    </form>
+    <>
+      <form className={styles.form}>
+        <Input type="text" name="username" labelName="Username / E-mail" />
+        <Input type="password" name="password" labelName="Password" />
+        <SubmitButton type="submit" name="Submit" />
+      </form>
+      <form className={styles.form}>
+        <input type="hidden" name="guest_mode" value="true" />
+        <CancelButton type="submit" name="Guest Mode" />
+      </form>
+    </>
   );
 }
 export default Login;
