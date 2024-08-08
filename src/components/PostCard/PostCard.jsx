95a0ecf11d267412cc5cb20eb8728ad94a8da660
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-08 18:29:23 +0200

    Split PostCard into Post, Comment & AddComment modules

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,7 +1,6 @@
 import styles from './PostCard.module.css';
-import Icon from '@mdi/react';
-import { mdiThumbUp, mdiMessage } from '@mdi/js';
-import Comment from '../Comment/Comment';
+import Post from './Post/Post';
+import Comment from './Comment/Comment';
 import AddComment from '../AddComment/AddComment';
 import { useEffect, useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
@@ -41,32 +40,8 @@ function PostCard() {
   
   return (
     <>
-      <div className={styles.post}>
-        <div className={styles.postInfo}>
-          <img src="https://i.pravatar.cc/45" alt="avatar" />
-          <div>
-            <p className={styles.name}>James Smith</p>
-            <p className={styles.date}>3 min ago</p>
-          </div>
-        </div>
-        <p className={styles.postContent}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
-          voluptate cum vero, similique qui ipsum nostrum nesciunt delectus
-          illum possimus vel nihil, iusto rem veritatis assumenda debitis sint
-          sunt enim.
-        </p>
-        <hr />
-        <ul className={styles.listIcons}>
-          <li className={styles.listItem}>
-            <Icon path={mdiThumbUp} size={1} />
-            Like
-          </li>
-          <li className={styles.listItem}>
-            <Icon path={mdiMessage} size={1} />
-            Comment
-          </li>
-        </ul>
-        <hr />
+      <div className={styles.postCard}>
+        <Post />
         <AddComment />
         <Comment />
       </div>
