24283cf7d88f4d460654ec650c0ba44fafd0b811
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-08 13:28:33 +0200

    Add fetch API in App.jsx & Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import { useEffect, useState } from 'react';
+import getRequestWithNativeFetch from './utils/fetchApiGet';
 
 function App() {
+  const [user, setUser] = useState({ _id: undefined, username: undefined });
+
+  const currentToken = localStorage.getItem('token');
+  const [token, setToken] = useState(currentToken);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      const fetchDataForUsers = async () => {
+        try {
+          const url = `${import.meta.env.VITE_BACKEND_URL}/user`;
+          const headers = {
+            Authorization: token,
+          };
+          const userData = await getRequestWithNativeFetch(url, headers);
+          setUser(userData);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      fetchDataForUsers();
+    }
+    return () => {
+      setUser([]);
+    };
+  }, [token]);
   return (
     <>
       <Navbar />
       <main>
-        <Outlet />
+        <Outlet context={[token, setToken, user, isLoading, setIsLoading]} />
       </main>
     </>
   );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,18 +3,67 @@ import SubmitButton from '../../components/Form/Buttons/SubmitButton';
 import CancelButton from '../../components/Form/Buttons/cancelButton';
 import styles from './Login.module.css';
 
+import { useState } from 'react';
+
+import requestWithNativeFetch from '../../utils/fetchApi';
+import { useNavigate, useOutletContext } from 'react-router-dom';
+
 function Login() {
+  const [fetchData, setFetchData] = useState(null);
+  const [token, setToken, , isLoading, setIsLoading] = useOutletContext();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const fetchDataForLogin = async () => {
+      try {
+        const url = `${import.meta.env.VITE_BACKEND_URL}/login`;
+        const headers = { 'Content-Type': 'application/json' };
+        const data = {
+          username: e.target.username.value,
+          password: e.target.password.value,
+        };
+        const messagesData = await requestWithNativeFetch(
+          url,
+          'POST',
+          headers,
+          data
+        );
+        setFetchData(messagesData);
+        setIsLoading(false);
+        if (messagesData.success) {
+          const dataToken = messagesData.token;
+          localStorage.setItem('token', dataToken);
+          setToken(dataToken);
+          navigate('/');
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchDataForLogin();
+  };
+
   return (
     <>
-      <form className={styles.form}>
-        <Input type="text" name="username" labelName="Username / E-mail" />
-        <Input type="password" name="password" labelName="Password" />
-        <SubmitButton type="submit" name="Submit" />
-      </form>
-      <form className={styles.form}>
-        <input type="hidden" name="guest_mode" value="true" />
-        <CancelButton type="submit" name="Guest Mode" />
-      </form>
+      {isLoading ? (
+        <Loader />
+      ) : !token ? (
+        <>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <Input type="text" name="username" labelName="Username / E-mail" />
+            <Input type="password" name="password" labelName="Password" />
+            <SubmitButton type="submit" name="Submit" />
+          </form>
+          <form className={styles.form}>
+            <input type="hidden" name="guest_mode" value="true" />
+            <CancelButton type="submit" name="Guest Mode" />
+          </form>
+        </>
+      ) : (
+        <p>You are logged in</p>
+      )}
     </>
   );
 }
