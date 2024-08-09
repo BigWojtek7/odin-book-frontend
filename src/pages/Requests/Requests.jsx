0433a38fcceeebdbbee85138ee5548c3098af6f5
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-09 08:49:04 +0200

    Add Fetch API for suggesting friends in Requests

--- src/pages/Requests/Requests.jsx
+++ src/pages/Requests/Requests.jsx
@@ -7,6 +7,7 @@ import { useOutletContext } from 'react-router-dom';
 import getRequestWithNativeFetch from '../../utils/fetchApiGet';
 function Requests() {
   const [requests, setRequests] = useState([]);
+  const [friendsSuggest, setFriendsSuggest] = useState([]);
   // const [deleteRes, setDeleteRes] = useState({});
   const [token, , user, isLoading, setIsLoading] = useOutletContext();
   console.log(user);
@@ -35,113 +36,98 @@ function Requests() {
       setRequests([]);
     };
   }, [setIsLoading, token, user]);
-  console.log(requests);
+
+  useEffect(() => {
+    if (user?.user_id) {
+      setIsLoading(true);
+
+      const fetchDataForMessages = async () => {
+        try {
+          const url = `${import.meta.env.VITE_BACKEND_URL}/users/all`;
+          const headers = {
+            Authorization: token,
+          };
+          const friendsData = await getRequestWithNativeFetch(url, headers);
+          setFriendsSuggest(friendsData);
+          setIsLoading(false);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      fetchDataForMessages();
+    }
+    return () => {
+      setRequests([]);
+    };
+  }, [setIsLoading, token, user]);
+  console.log(friendsSuggest);
   return (
     <div className={styles.container}>
       <div className={styles.sideCard}>
         <h2 className={styles.sideHeading}>Following Requests:</h2>
-        <div className={styles.usersCard}>
-          {requests.map((request) => (
-            <>
-              <Friend
-                name={request.follower_name}
-                friendsNumber={request.user_followers_count}
-                avatarURL={request.avatar_url}
-                key={request.id}
+        {requests.map((request) => (
+          <div className={styles.usersCard} key={request.id}>
+            <Friend
+              name={request.follower_name}
+              friendsNumber={request.user_followers_count}
+              avatarURL={request.avatar_url}
+            />
+            <form className={styles.form}>
+              <input type="hidden" name="confirm_friend" value={'34657'} />
+              <SubmitButton
+                type="submit"
+                name="Confirm"
+                style={{
+                  fontSize: '1rem',
+                  marginLeft: '0.5em',
+                  borderRadius: '10px',
+                }}
               />
-              <form className={styles.form}>
-                <input type="hidden" name="confirm_friend" value={'34657'} />
-                <SubmitButton
-                  type="submit"
-                  name="Confirm"
-                  style={{
-                    fontSize: '1rem',
-                    marginLeft: '0.5em',
-                    borderRadius: '10px',
-                  }}
-                />
-              </form>
-              <form className={styles.form}>
-                <input type="hidden" name="delete_request" value="34657" />
-                <CancelButton
-                  type="submit"
-                  name="Delete"
-                  style={{
-                    fontSize: '1rem',
-                    marginLeft: '0.5em',
-                    borderRadius: '10px',
-                  }}
-                />
-              </form>
-            </>
-          ))}
-        </div>
+            </form>
+            <form className={styles.form}>
+              <input type="hidden" name="delete_request" value="34657" />
+              <CancelButton
+                type="submit"
+                name="Delete"
+                style={{
+                  fontSize: '1rem',
+                  marginLeft: '0.5em',
+                  borderRadius: '10px',
+                }}
+              />
+            </form>
+          </div>
+        ))}
       </div>
       <div className={styles.sideCard}>
         <h2 className={styles.sideHeading}>Users you may know:</h2>
-        <div className={styles.usersCard}>
-          <Friend />
-          <SubmitButton
-            type="submit"
-            name="Confirm"
-            style={{
-              fontSize: '1rem',
-              marginLeft: '0.5em',
-              borderRadius: '10px',
-            }}
-          />
-          <CancelButton
-            type="submit"
-            name="Delete"
-            style={{
-              fontSize: '1rem',
-              marginLeft: '0.5em',
-              borderRadius: '10px',
-            }}
-          />
-        </div>
-        <div className={styles.usersCard}>
-          <Friend />
-          <SubmitButton
-            type="submit"
-            name="Confirm"
-            style={{
-              fontSize: '1rem',
-              marginLeft: '0.5em',
-              borderRadius: '10px',
-            }}
-          />
-          <CancelButton
-            type="submit"
-            name="Delete"
-            style={{
-              fontSize: '1rem',
-              marginLeft: '0.5em',
-              borderRadius: '10px',
-            }}
-          />
-        </div>{' '}
-        <div className={styles.usersCard}>
-          <Friend />
-          <SubmitButton
-            type="submit"
-            name="Confirm"
-            style={{
-              fontSize: '1rem',
-              marginLeft: '0.5em',
-              borderRadius: '10px',
-            }}
-          />
-          <CancelButton
-            type="submit"
-            name="Delete"
-            style={{
-              fontSize: '1rem',
-              marginLeft: '0.5em',
-              borderRadius: '10px',
-            }}
-          />
-        </div>
+        {friendsSuggest.map((friend) => (
+          <div className={styles.usersCard} key={friend.user_id}>
+            <Friend
+              name={friend.full_name}
+              friendsNumber={friend.user_followers_count}
+              avatarURL={friend.avatar_url}
+            />
+            <SubmitButton
+              type="submit"
+              name="Confirm"
+              style={{
+                fontSize: '1rem',
+                marginLeft: '0.5em',
+                borderRadius: '10px',
+              }}
+            />
+            <CancelButton
+              type="submit"
+              name="Delete"
+              style={{
+                fontSize: '1rem',
+                marginLeft: '0.5em',
+                borderRadius: '10px',
+              }}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
