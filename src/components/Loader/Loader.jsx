3d3bb49d345704041fcd2ed6231d5e5c0d12f52c
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-08-19 21:40:48 +0200

    Add adnotation on server loadin in loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,8 @@ import styles from './Loader.module.css'
 function Loader(){
   return(
     <div className={styles.loader}>
-    <p>Data is loading...</p>
+    <p className={styles.loading}>Data is loading...</p>
+    <p className={styles.info}>Loading the first time may take longer - the server is waking up</p>
     <div className={styles.spinner}></div>
     </div>
   )
