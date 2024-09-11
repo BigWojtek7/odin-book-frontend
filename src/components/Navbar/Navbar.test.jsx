ea31fb1fb0347fa73e1d78e23571c5c62b4e705e
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-09-11 20:10:01 +0200

    Add test for Navbar components

---
+++ src/components/Navbar/Navbar.test.jsx
@@ -0,0 +1,17 @@
+import { expect } from 'vitest';
+import Navbar from './Navbar';
+
+
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('Testing Navbar Components', () => {
+  it('The component UI should not change', () => {
+    const { container } = render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
