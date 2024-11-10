d55f50c9149cf8c8e330db4c714233a57c68809b
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-11-10 08:45:08 +0100

    Add loaderProvider with hooks useLoader

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,44 +6,39 @@ import { useState } from 'react';
 import Icon from '@mdi/react';
 import { mdiLogin, mdiAccountPlus } from '@mdi/js';
 
-
 function Home() {
   const [unFollowReq, setUnFollowReq] = useState({});
-  const [token, , user, ] = useOutletContext();
+  const [token, , user] = useOutletContext();
   return (
-        <>
-          {token ? (
-            <div className={styles.home}>
-              <FriendsCard
-                unFollowReq={unFollowReq}
-                setUnFollowReq={setUnFollowReq}
-              />
-              <div className={styles.posts}>
-                <PostCard
-                  forceRenderPosts={unFollowReq}
-                  fetchUrl={`${import.meta.env.VITE_BACKEND_URL}/posts/${
-                    user.user_id
-                  }/followers`}
-                  profileUser={user}
-                />
-              </div>
-            </div>
-          ) : (
-            <div className={styles.welcomeMessage}>
-              <h1>Welcome to Odin Book</h1>
-              <Link className={styles.login} to="/login">
-                <Icon path={mdiLogin} size={5} color="var(--input-bd)"></Icon>
-              </Link>
-              <Link className={styles.signUp} to="/sign-up">
-                <Icon
-                  path={mdiAccountPlus}
-                  size={5}
-                  color="var(--btn-clr)"
-                ></Icon>
-              </Link>
-            </div>
-          )}
-        </>
+    <>
+      {token ? (
+        <div className={styles.home}>
+          <FriendsCard
+            unFollowReq={unFollowReq}
+            setUnFollowReq={setUnFollowReq}
+          />
+          <div className={styles.posts}>
+            <PostCard
+              forceRenderPosts={unFollowReq}
+              fetchUrl={`${import.meta.env.VITE_BACKEND_URL}/posts/${
+                user.user_id
+              }/followers`}
+              profileUser={user}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className={styles.welcomeMessage}>
+          <h1>Welcome to Odin Book</h1>
+          <Link className={styles.login} to="/login">
+            <Icon path={mdiLogin} size={5} color="var(--input-bd)"></Icon>
+          </Link>
+          <Link className={styles.signUp} to="/sign-up">
+            <Icon path={mdiAccountPlus} size={5} color="var(--btn-clr)"></Icon>
+          </Link>
+        </div>
+      )}
+    </>
   );
 }
 export default Home;
