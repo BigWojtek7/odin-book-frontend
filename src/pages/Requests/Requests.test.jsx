53c055797cba926c143ab5db21948c68fddf3e08
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-12-08 21:22:48 +0100

    Refactor tests for Requests page

--- src/pages/Requests/Requests.test.jsx
+++ src/pages/Requests/Requests.test.jsx
@@ -1,5 +1,4 @@
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithProviders from '../../utils/testHelpers/renderWithProviders';
 import Requests from './Requests';
 import useRequests from '../../hooks/useRequests';
@@ -64,42 +63,5 @@ describe('Requests Component', () => {
     expect(screen.getByText('Users you may know:')).toBeInTheDocument();
     expect(screen.getByText('Alice Johnson')).toBeInTheDocument();
     expect(screen.getByText('Send follow request')).toBeInTheDocument();
-  });
-
-  it('calls handleAddFollower when Confirm button is clicked', async () => {
-    const user = userEvent.setup();
-    const { handleAddFollower } = useRequests.mock.results[0].value;
-
-    renderWithProviders(<Requests />);
-
-    const confirmButton = screen.getByText('Confirm');
-    await user.click(confirmButton);
-
-    expect(handleAddFollower).toHaveBeenCalledWith(1); // Sprawdzanie follower_id
-  });
-
-  it('calls handleDeleteRequest for Sent Requests when Cancel button is clicked', async () => {
-    const user = userEvent.setup();
-
-    const { handleDeleteRequest } = useRequests.mock.results[0].value;
-    renderWithProviders(<Requests />);
-
-    const cancelButton = screen.getByText('Cancel follow request');
-    await user.click(cancelButton);
-
-    expect(handleDeleteRequest).toHaveBeenCalledWith('sent', 2); // Typ requestu i follower_id
-  });
-
-  it('calls handleSentRequest when Send follow request button is clicked', async () => {
-    const user = userEvent.setup();
-    console.log(useRequests.mock.results)
-    const { handleSentRequest } = useRequests.mock.results[0].value;
-
-    renderWithProviders(<Requests />);
-
-    const sendRequestButton = screen.getByText('Send follow request');
-    await user.click(sendRequestButton);
-
-    expect(handleSentRequest).toHaveBeenCalledWith(3); // ID użytkownika
   });
 });
